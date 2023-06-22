188cbc1c04fbc7a7f17c37c91bb98c2e167abad5
hossammenem <hossammenem0102840@gmail.com>
2023-06-22 20:08:14 +0300

    adding 'log message' functionality

--- src/logEmmeter.ts
+++ src/logEmmeter.ts
@@ -2,12 +2,11 @@ import * as vscode from "vscode";
 import * as emmet from "./logFuntions/index";
 import isVariable from "./logFuntions/varFunctions/isVariable";
 
-export default function logEmmter() {
+export default async function logEmmter() {
   const activeEditor = vscode.window.activeTextEditor;
   if (activeEditor) {
     const currentPosition = activeEditor.selection.active;
     const currentLine = activeEditor.document.lineAt(currentPosition.line);
-
     const lineStart = new vscode.Position(currentLine.lineNumber, 0);
     const lineEnd = new vscode.Position(
       currentLine.lineNumber,
@@ -17,20 +16,22 @@ export default function logEmmter() {
     const lineRange = new vscode.Range(lineStart, lineEnd);
 
     const textContent = currentLine.text.replace("//", "").trim();
-
     const colonIdx = textContent.indexOf(":");
     const n = textContent.length;
     const option = textContent.slice(colonIdx + 1, n).trim();
     const vars = textContent.slice(0, colonIdx > 0 ? colonIdx : n).split(",");
 
-    if (isVariable(textContent)) {
-      emmet.varDef(
-        activeEditor,
-        activeEditor.document,
-        textContent,
-        currentPosition
-      );
-    } else if (textContent) {
+    if (textContent.match(/^".*"$/g)) {
+      const currentIndentation = currentLine.text.match(/^\s*/)?.[0] || "";
+      activeEditor.edit((editBuilder) => {
+        editBuilder.replace(
+          lineRange,
+          currentIndentation + `console.log(${textContent});`
+        );
+      });
+    } else if (await isVariable(activeEditor.document, currentPosition)) {
+      emmet.varDef(activeEditor, activeEditor.document, currentPosition);
+    } else if (textContent.match(/^[A-Za-z0-9,: ]+$/s)) {
       emmet.emmetVars(activeEditor, vars, lineRange, option.toLowerCase());
     }
   }
